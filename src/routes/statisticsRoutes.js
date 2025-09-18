8f507984ab7631c79af187773b9c970ed4d41765
Sebastian Cerquera <sebartin9@gmail.com>
2025-09-18 09:34:16 -0500

    eliminacion del backend antiguo y creacion del nuevo

--- src/routes/statisticsRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getStatistics } from '../controllers/statisticsController.js';
-
-const router = Router();
-
-// Ruta de solo lectura para obtener estadísticas
-router.get('/statistics', getStatistics);
-
-export default router;
