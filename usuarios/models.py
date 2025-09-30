4e80b47e0a080fd4d279bbf9fd10c6a1ad9d0a84
pendracoV <gaby.trivino@hotmail.com>
2025-09-30 15:37:13 -0500

    arreglo de campos para el usuario y migraciones

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,16 +1,16 @@
+# usuarios/models.py
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here
 class CustomUser(AbstractUser):
     """
     Custom user abstraido de django
     """
     first_name = None
     last_name = None
+    username = models.CharField(max_length=150, unique=True)  # ← Asegurar que esté
     name = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "name"]
-    
