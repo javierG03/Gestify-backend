6fa84b51bd84ed4c61733219d7ca1df996882c58
Ubuntu <ubuntu@ip-172-31-16-9.ec2.internal>
2025-11-21 19:57:11 +0000

    Merge solucionando errores al mostrar el panel administrativo

--- gestify/settings.py
+++ gestify/settings.py
@@ -143,6 +143,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -283,3 +284,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # URLs para PayU callbacks
 BACKEND_BASE_URL = os.getenv('BACKEND_BASE_URL', 'http://localhost:8000')
 FRONTEND_BASE_URL = os.getenv('FRONTEND_BASE_URL', 'http://localhost:3000')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
